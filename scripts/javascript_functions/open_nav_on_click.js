8a1b604db0c13262748eb0a28425430fe1b1e6bd
Prajwal <prajwaldwivedi257@gmail.com>
2020-08-29 17:41:03 +0530

    v 0.96
    navigation scrolling in Xasis is fixed now.

--- scripts/javascript_functions/open_nav_on_click.js
+++ scripts/javascript_functions/open_nav_on_click.js
@@ -7,14 +7,16 @@ export var open_nav_on_click = function(){
     var toggleNav = function(){
         if(flag==false){
             navigation.style.clipPath = "polygon(0 0, 100% 0, 100% 100%, 0% 100%)";
-            navigation_trigger.style.position = "fixed";
+            //navigation_trigger.style.position = "fixed";
+            document.body.style.overflowY = "hidden";
             nav_icon_line_1.classList.add("line_1_active")
             nav_icon_line_2.classList.add("line_2_active")
             flag=true;
         }
         else{
             navigation.style.clipPath = "polygon(100% 0, 100% 0, 100% 100%, 100% 100%)";
-            navigation_trigger.style.position = "relative";
+            document.body.style.overflowY = "visible";
+            //navigation_trigger.style.position = "relative";
             nav_icon_line_1.classList.remove("line_1_active")
             nav_icon_line_2.classList.remove("line_2_active")
             flag=false;
