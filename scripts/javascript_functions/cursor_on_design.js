a83af7d6045d7c3e8ce333791b9995215d392665
Prajwal <prajwaldwivedi257@gmail.com>
2020-10-06 12:38:30 +0530

    v 0.9.2

--- scripts/javascript_functions/cursor_on_design.js
+++ scripts/javascript_functions/cursor_on_design.js
@@ -6,33 +6,33 @@ export var cursor_on_design = function(){
     var view_on_hover_project = document.getElementsByClassName("design-1")[0];
     view_on_hover_project.addEventListener("mouseenter", function(){
         cursor_project.style.backgroundImage = "url('/assets/images/design-1.png')";
-        cursor_project.style.transform = "scaleY(1)";
+        cursor_project.style.transform = "scale(1)";
         document.body.style.backgroundColor = "#D2D2D2";
     });
     view_on_hover_project.addEventListener("mouseleave", function(){
-        cursor_project.style.transform = "scaleY(0)";
+        cursor_project.style.transform = "scale(0)";
         document.body.style.backgroundColor = "#f5f5f5";
     });
 
     var view_on_hover_project = document.getElementsByClassName("design-2")[0];
     view_on_hover_project.addEventListener("mouseenter", function(){
         cursor_project.style.backgroundImage = "url('/assets/images/design-2.png')";
-        cursor_project.style.transform = "scaleY(1)";
+        cursor_project.style.transform = "scale(1)";
         document.body.style.backgroundColor = "#D2D2D2";
     });
     view_on_hover_project.addEventListener("mouseleave", function(){
-        cursor_project.style.transform = "scaleY(0)";
+        cursor_project.style.transform = "scale(0)";
         document.body.style.backgroundColor = "#f5f5f5";
     });
 
     var view_on_hover_project = document.getElementsByClassName("design-3")[0];
     view_on_hover_project.addEventListener("mouseenter", function(){
         cursor_project.style.backgroundImage = "url('/assets/images/design-3.png')";
-        cursor_project.style.transform = "scaleY(1)";
+        cursor_project.style.transform = "scale(1)";
         document.body.style.backgroundColor = "#D2D2D2";
     });
     view_on_hover_project.addEventListener("mouseleave", function(){
-        cursor_project.style.transform = "scaleY(0)";
+        cursor_project.style.transform = "scale(0)";
         document.body.style.backgroundColor = "#f5f5f5";
     });
 
