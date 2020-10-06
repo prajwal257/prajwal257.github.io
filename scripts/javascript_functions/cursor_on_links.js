a056e2a6c6416ab217c78bd68cd9d4abed943b2b
Prajwal <prajwaldwivedi257@gmail.com>
2020-10-06 12:00:06 +0530

    v 0.9.2

--- scripts/javascript_functions/cursor_on_links.js
+++ scripts/javascript_functions/cursor_on_links.js
@@ -5,10 +5,12 @@ export var cursor_on_links = function(){
         document.getElementsByClassName("about_page_link")[i].addEventListener("mouseover",function(e){
             console.log("scaling the cursor up");
             cursor.classList.add("cursor_on_links");
+            document.getElementsByClassName("cursor_follower")[0].style.transition = "all .025s";
         });
         document.getElementsByClassName("about_page_link")[i].addEventListener("mouseout",function(e){
             console.log("scaling the cursor down");
             cursor.classList.remove("cursor_on_links");
+            document.getElementsByClassName("cursor_follower")[0].style.transition = "all .1s";
         });
     }
 }
