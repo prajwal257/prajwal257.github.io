d446a85db2b6c9f9d31c5989f5e0480d68ba4bde
prajwal257 <69119435+prajwal257@users.noreply.github.com>
2023-11-02 00:11:43 +0530

    Update for performance.

--- js/index_page_scripts.js
+++ js/index_page_scripts.js
@@ -1,11 +1,13 @@
 import {projects} from "./scripts/projects.js";
 import { recolor_background_blobs } from "./scripts/recolor.js";
+import { recolor_background_blobs_now } from "./scripts/recolor.js";
 // import { panzoom } from "./scripts/panzoom.js";
 
 export function home_page_scripts(){
     try{
         projects();
         recolor_background_blobs()
+        setInterval(()=>{recolor_background_blobs_now()}, 5000)
     }catch(error){
         console.log("This is not the projects page or background_blobs or panzoom.", error)
     }
--- js/scripts/recolor.js
+++ js/scripts/recolor.js
@@ -8,3 +8,10 @@ export function recolor_background_blobs(){
         }
     })
 }
+export function recolor_background_blobs_now(){
+    const background_blobs = document.getElementsByClassName("background_blob")
+    const random = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+    for(let i=0; i<background_blobs.length; i++){
+        background_blobs[i].style.backgroundColor = `rgb(${random(0, 255)}, ${random(0, 255)}, ${random(0, 255)})`;
+    }
+}
